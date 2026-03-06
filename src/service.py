fbad1aecc3721c79c5db0745fd06bd64bbe40b2b
27Nyappy <antonella.gr27@gmail.com>
2026-03-05 20:54:30 -0800

    need try/except

--- src/service.py
+++ src/service.py
@@ -26,103 +26,109 @@ app.add_middleware(
 
 @app.post("/increase")
 async def increase_stat(request: Request):
-	body = await request.json()
-	encrypted_state = body.get("state")
-	stat = body.get("stat")
-	auth = request.headers.get("Authorization")
+	try:
+		body = await request.json()
+		encrypted_state = body.get("state")
+		stat = body.get("stat")
+		auth = request.headers.get("Authorization")
 
-	if not encrypted_state or not stat:
-		return JSONResponse(status_code=400, content={"error": "Invalid request"})
-	
-	async with httpx.AsyncClient(verify="certificate.pem") as client:
-		if not auth:
-				decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", params={"state": encrypted_state})
-		else:
-				decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", headers={"Authorization": auth}, params={"state": encrypted_state})
-
-		if decrypt_res.status_code != 200:
-			return JSONResponse(status_code=decrypt_res.status_code, content={"error": decrypt_res.json().get("error")})
-
-		res_data = decrypt_res.json().get("data")
-		mood = res_data["pet"]["moodMeter"]
-
-		if stat not in mood:
+		if not encrypted_state or not stat:
 			return JSONResponse(status_code=400, content={"error": "Invalid request"})
+		
+		async with httpx.AsyncClient(verify="certificate.pem") as client:
+			if not auth:
+					decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", params={"state": encrypted_state})
+			else:
+					decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", headers={"Authorization": auth}, params={"state": encrypted_state})
 
-		curr_stat_val = mood[stat]
+			if decrypt_res.status_code != 200:
+				return JSONResponse(status_code=decrypt_res.status_code, content={"error": decrypt_res.json().get("error")})
 
-		if curr_stat_val > 75:
-			return JSONResponse(status_code=200, content={"message": "No stat updates"})
+			res_data = decrypt_res.json().get("data")
+			mood = res_data["pet"]["moodMeter"]
 
-		updated_data = {"pet": {"moodMeter": {stat: curr_stat_val + 25}}}
-		params = {"state": encrypted_state}
-		headers = {"Authorization": auth} if auth else {}
+			if stat not in mood:
+				return JSONResponse(status_code=400, content={"error": "Invalid request"})
 
-		update_res = await client.post(f"{config.get('STATE_VAULT_HOST')}/update", headers=headers, params=params, json=updated_data)
+			curr_stat_val = mood[stat]
 
-		if update_res.status_code != 200:
-			return JSONResponse(status_code=update_res.status_code, content={"error": update_res.json().get("error")})
+			if curr_stat_val > 75:
+				return JSONResponse(status_code=200, content={"message": "No stat updates"})
 
-		update_res_json = update_res.json()
-		return JSONResponse(status_code=200, content={
-			"encrypted": update_res_json.get("encrypted"),
-			"data": update_res_json.get("data"),
-			"quantity": 25
-			})
+			updated_data = {"pet": {"moodMeter": {stat: curr_stat_val + 25}}}
+			params = {"state": encrypted_state}
+			headers = {"Authorization": auth} if auth else {}
+
+			update_res = await client.post(f"{config.get('STATE_VAULT_HOST')}/update", headers=headers, params=params, json=updated_data)
+
+			if update_res.status_code != 200:
+				return JSONResponse(status_code=update_res.status_code, content={"error": update_res.json().get("error")})
+
+			update_res_json = update_res.json()
+			return JSONResponse(status_code=200, content={
+				"encrypted": update_res_json.get("encrypted"),
+				"data": update_res_json.get("data"),
+				"quantity": 25
+				})
+	except:
+		return JSONResponse(status_code=400, content={"error": "Invalid request"})
 
 @app.post("/stats-decay")
 async def stats_decay(request: Request):
-	body = await request.json()
-	encrypted_state = body.get("state")
-	auth = request.headers.get("Authorization")
+	try:
+		body = await request.json()
+		encrypted_state = body.get("state")
+		auth = request.headers.get("Authorization")
 
-	if not encrypted_state:
+		if not encrypted_state:
+			return JSONResponse(status_code=400, content={"error": "Invalid request"})
+		
+		
+		async with httpx.AsyncClient(verify="certificate.pem") as client:
+			if not auth:
+					decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", params={"state": encrypted_state})
+			else:
+					decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", headers={"Authorization": auth}, params={"state": encrypted_state})
+
+			if decrypt_res.status_code != 200:
+				return JSONResponse(status_code=decrypt_res.status_code, content={"error": decrypt_res.json().get("error")})
+
+			res_data = decrypt_res.json().get("data")
+			mood = res_data["pet"]["moodMeter"]
+			last_updated = datetime.fromisoformat(mood["lastStatDecrement"].replace("Z", "+00:00"))
+			current_time = datetime.now(timezone.utc)
+			decrease_by = timed_stat_decrease(last_updated, current_time)
+
+			if decrease_by == 0:
+				return JSONResponse(status_code=200, content={"message": "No stat updates"})
+
+			stat_updates = {}
+			for stat, val in mood.items():
+				if stat != "lastStatDecrement" and val >= 25:
+					new_val = val - decrease_by
+					stat_updates[stat] = max(0, val - decrease_by)
+
+			if not stat_updates:
+				return JSONResponse(status_code=200, content={"message": "No stat updates"})
+
+			stat_updates["lastStatDecrement"] = current_time.isoformat().replace("+00:00", "Z")
+			updated_data = {"pet": {"moodMeter": stat_updates}}
+			params = {"state": encrypted_state}
+			headers = {"Authorization": auth} if auth else {}
+
+			update_res = await client.post(f"{config.get('STATE_VAULT_HOST')}/update", headers=headers, params=params, json=updated_data)
+
+			if update_res.status_code != 200:
+				return JSONResponse(status_code=update_res.status_code, content={"error": update_res.json().get("error")})
+
+			update_res_json = update_res.json()
+			return JSONResponse(status_code=200, content={
+				"encrypted": update_res_json.get("encrypted"),
+				"data": update_res_json.get("data"),
+				"quantity": -abs(decrease_by)
+				})
+	except:
 		return JSONResponse(status_code=400, content={"error": "Invalid request"})
-	
-	
-	async with httpx.AsyncClient(verify="certificate.pem") as client:
-		if not auth:
-				decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", params={"state": encrypted_state})
-		else:
-				decrypt_res = await client.get(f"{config.get('STATE_VAULT_HOST')}/decrypt", headers={"Authorization": auth}, params={"state": encrypted_state})
-
-		if decrypt_res.status_code != 200:
-			return JSONResponse(status_code=decrypt_res.status_code, content={"error": decrypt_res.json().get("error")})
-
-		res_data = decrypt_res.json().get("data")
-		mood = res_data["pet"]["moodMeter"]
-		last_updated = datetime.fromisoformat(mood["lastStatDecrement"].replace("Z", "+00:00"))
-		current_time = datetime.now(timezone.utc)
-		decrease_by = timed_stat_decrease(last_updated, current_time)
-
-		if decrease_by == 0:
-			return JSONResponse(status_code=200, content={"message": "No stat updates"})
-
-		stat_updates = {}
-		for stat, val in mood.items():
-			if stat != "lastStatDecrement" and val >= 25:
-				new_val = val - decrease_by
-				stat_updates[stat] = max(0, val - decrease_by)
-
-		if not stat_updates:
-			return JSONResponse(status_code=200, content={"message": "No stat updates"})
-
-		stat_updates["lastStatDecrement"] = current_time.isoformat().replace("+00:00", "Z")
-		updated_data = {"pet": {"moodMeter": stat_updates}}
-		params = {"state": encrypted_state}
-		headers = {"Authorization": auth} if auth else {}
-
-		update_res = await client.post(f"{config.get('STATE_VAULT_HOST')}/update", headers=headers, params=params, json=updated_data)
-
-		if update_res.status_code != 200:
-			return JSONResponse(status_code=update_res.status_code, content={"error": update_res.json().get("error")})
-
-		update_res_json = update_res.json()
-		return JSONResponse(status_code=200, content={
-			"encrypted": update_res_json.get("encrypted"),
-			"data": update_res_json.get("data"),
-			"quantity": -abs(decrease_by)
-			})
 
 
 def main():
